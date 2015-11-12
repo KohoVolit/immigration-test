a3e344e25738848d227f56919ceaf22bf7b31e1e
Michal Skop <michal.skop@kohovolit.eu>
2015-11-12 04:58:19 +0100

    settings example

---
+++ settings_example.php
@@ -0,0 +1,17 @@
+<?php
+//settings - example
+//should be copied into setting.php
+
+//full path to Smarty.class.php
+const SMARTY_PATH = "/usr/local/lib/php/Smarty/Smarty.class.php";
+
+//full path to the application
+const APP_PATH = "/home/shared/immigration-test/";
+
+//url of the application
+const APP_URL = "http://test.kohovolit.eu/imigranti-2015/";
+
+//url of cdn
+const CDN_URL = "http://test.kohovolit.eu/imigranti-2015/";
+
+?>
